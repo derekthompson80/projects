1321cb2075944200a9803d68430e0a00e7bc643f
spade <spade605@gmail.com>
2025-12-16 20:28:11 -0500

    changing port

--- Blog/blog_db.py
+++ Blog/blog_db.py
@@ -20,7 +20,7 @@ if load_dotenv:
 # Database Configuration (SINGLE SOURCE OF TRUTH)
 # --------------------------------------------------
 DB_HOST = os.getenv("DB_HOST", "127.0.0.1")
-DB_PORT = int(os.getenv("DB_PORT", "3306"))
+DB_PORT = int(os.getenv("DB_PORT", "3305"))
 DB_USER = os.getenv("DB_USER")
 DB_PASSWORD = os.getenv("DB_PASSWORD")
 DB_NAME = os.getenv("DB_NAME")
